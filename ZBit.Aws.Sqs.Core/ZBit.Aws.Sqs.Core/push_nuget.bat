c85ec2c9092777cacb8a5e04bdad1115f0d91645
gzinger <gzinger@acats.com>
2018-09-18 07:57:36 -0400

    changed version to 1.2

--- ZBit.Aws.Sqs.Core/ZBit.Aws.Sqs.Core/push_nuget.bat
+++ ZBit.Aws.Sqs.Core/ZBit.Aws.Sqs.Core/push_nuget.bat
@@ -1,5 +1,5 @@
 #C:\Users\gzinger\Downloads\DevTools\nuget pack ZBit.Aws.Sqs.Core.csproj -Prop Configuration=Release -Symbols
 #pause
-C:\Users\gzinger\Downloads\DevTools\nuget push bin\release\ZBit.Aws.Sqs.Core.1.0.1.nupkg -Source https://www.nuget.org/api/v2/package
+C:\Users\gzinger\Downloads\DevTools\nuget push bin\release\ZBit.Aws.Sqs.Core.1.2.0.nupkg -Source https://www.nuget.org/api/v2/package
 pause
 #nuget push ZBit.Aws.Sqs.Core.1.0.1.symbols.nupkg
